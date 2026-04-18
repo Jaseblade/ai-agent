9df379f583f56ad70080b6fa7458c4612f4281a8
Jaseblade <jasonabisaab@gmail.com>
2026-04-18 09:18:30 +0100

    added testing for write_file functionality

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,28 +1,32 @@
 import os
 from config import MAX_CHARS
 
+
 # print(MAX_CHARS)
 def get_file_content(working_directory, file_path):
     working_dir_path = os.path.abspath(working_directory)
     target_file_path = os.path.normpath(os.path.join(working_dir_path, file_path))
-    
+
     # will be True or False
-    valid_target_file_path = os.path.commonpath([working_dir_path, target_file_path]) == working_dir_path
+    valid_target_file_path = (
+        os.path.commonpath([working_dir_path, target_file_path]) == working_dir_path
+    )
 
     if not valid_target_file_path:
         return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory'
 
     if not os.path.isfile(target_file_path):
         return f'Error: File not found or is not a regular file: "{file_path}"'
-    
+
     try:
         with open(target_file_path, "r") as f:
             file_content_string = f.read(MAX_CHARS)
 
             if f.read(1):
-                file_content_string += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                file_content_string += (
+                    f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+                )
 
         return file_content_string
     except Exception as e:
-        return f'Error: {e}'
-    
+        return f"Error: {e}"
--- test_get_file_content.py
+++ test_get_file_content.py
@@ -1,12 +1,20 @@
 from functions.get_file_content import get_file_content
 
 
-print("Result for 'lorem.txt' file:", f"\n{get_file_content("calculator", "lorem.txt")}")
+print(
+    "Result for 'lorem.txt' file:", f"\n{get_file_content("calculator", "lorem.txt")}"
+)
 print()
 print("Result for 'main.py' file:", f"\n{get_file_content("calculator", "main.py")}")
 print()
-print("Result for 'pkg/calculator.py' file:", f"\n{get_file_content("calculator", "pkg/calculator.py")}")
+print(
+    "Result for 'pkg/calculator.py' file:",
+    f"\n{get_file_content("calculator", "pkg/calculator.py")}",
+)
 print()
 print("Result for '/bin/cat' file:", f"\n{get_file_content("calculator", "/bin/cat")}")
 print()
-print("Result for 'pkg/does_not_exist.py' file:", f"\n{get_file_content("calculator", "pkg/does_not_exist.py")}")
+print(
+    "Result for 'pkg/does_not_exist.py' file:",
+    f"\n{get_file_content("calculator", "pkg/does_not_exist.py")}",
+)
