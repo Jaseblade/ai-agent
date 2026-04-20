cc0742b4b949a60cebb7d8503edcede767fd0b2b
Jaseblade <jasonabisaab@gmail.com>
2026-04-20 19:40:40 +0100

    added a system prompt

--- main.py
+++ main.py
@@ -4,14 +4,19 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
 
 
-def generate_content(client, messages):
+def generate_content(client, messages, system_prompt):
     response = client.models.generate_content(
-        model='gemini-2.5-flash', 
-        contents=messages
+        model="gemini-2.5-flash",
+        contents=messages,
+        config=types.GenerateContentConfig(
+            system_instruction=system_prompt, temperature=0
+        ),
     )
     return response
+
 
 def main():
     parser = argparse.ArgumentParser(description="AI Agent")
@@ -21,29 +26,28 @@ def main():
 
     args = parser.parse_args()
 
-
     load_dotenv()
     try:
         api_key = os.environ.get("GEMINI_API_KEY")
     except:
         raise RuntimeError("The GEMINI_API_KEY environment variable was not found")
 
-
     client = genai.Client(api_key=api_key)
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    response = generate_content(client, messages)
+    response = generate_content(client, messages, system_prompt)
 
     # print(f"User prompt: {response.contents}")
     if response.usage_metadata is None:
         raise RuntimeError("No response usage metadata available. Request failed")
-    
+
     if args.verbose:
         print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-        
+
     print("Response:")
     print(response.text)
+
 
 if __name__ == "__main__":
     main()
