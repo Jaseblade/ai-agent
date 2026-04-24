9b6df5db817385301dfe1594fb9ee71b00867c67
Jaseblade <jasonabisaab@gmail.com>
2026-04-24 10:44:22 +0100

    completed aiagent course

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import argparse
 
 from dotenv import load_dotenv
@@ -35,37 +36,50 @@ def main():
 
     client = genai.Client(api_key=api_key)
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    response = generate_content(client, messages)
 
-    # print(f"User prompt: {response.contents}")
-    if response.usage_metadata is None:
-        raise RuntimeError("No response usage metadata available. Request failed")
+    for _ in range(20):
+        response = generate_content(client, messages)
 
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        # print(f"User prompt: {response.contents}")
+        if response.usage_metadata is None:
+            raise RuntimeError("No response usage metadata available. Request failed")
 
-    function_results = []
+        if response.candidates:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
 
-    if response.function_calls:
-        for function in response.function_calls:
-            function_call_result = call_function(function, verbose=args.verbose)
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-            if function_call_result.parts is None:
-                raise Exception
+        function_results = []
 
-            if function_call_result.parts[0].function_response is None:
-                raise Exception
+        if response.function_calls:
+            for function in response.function_calls:
+                function_call_result = call_function(function, verbose=args.verbose)
 
-            function_results.append(function_call_result.parts[0])
+                if function_call_result.parts is None:
+                    raise Exception
 
-            if args.verbose:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
+                if function_call_result.parts[0].function_response is None:
+                    raise Exception
 
-    else:
-        print("Response:")
-        print(response.text)
+                function_results.append(function_call_result.parts[0])
+
+                if args.verbose:
+                    print(
+                        f"-> {function_call_result.parts[0].function_response.response}"
+                    )
+
+            messages.append(types.Content(role="user", parts=function_results))
+
+        else:
+            print("Response:")
+            print(response.text)
+            return
+
+    sys.exit(1)
 
 
 if __name__ == "__main__":
